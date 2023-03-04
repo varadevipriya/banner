3719ac6116a71259c68da86502f63b38eb850559
priya <devipriya.akkineni123@gmail.com>
2023-03-04 04:48:59 +0000

    banner

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import BannerCardItem from './components/BannerCardItem'
+
+import './App.css'
+
+const bannerCardLists = [
+  {
+    id: 1,
+    headerText: 'The Seasons Latest',
+    description: 'Get the seasons all latest designs in a flick of your hand',
+    className: 'card-1',
+  },
+  {
+    id: 2,
+    headerText: 'Our New Designs',
+    description:
+      'Get the designs developed by our in-house team all for yourself',
+    className: 'card-2',
+  },
+  {
+    id: 3,
+    headerText: 'Insiders',
+    description: 'Get the top class products for yourself with an extra off',
+    className: 'card-3',
+  },
+]
+const App = () => (
+  <div className="app-container">
+    <ul className="banner-list-container">
+      {bannerCardLists.map(eachCard => (
+        <BannerCardItem bannerDetails={eachCard} key={eachCard.id} />
+      ))}
+    </ul>
+  </div>
+)
+export default App
